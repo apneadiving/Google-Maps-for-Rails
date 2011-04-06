4caf078b925f3e04cb14dfaaa78da0f80f8a81a6
Carl Sverre <mail@carlsverre.com>
2011-04-06 15:15:26 -0700

    Allow user to disable embedding gmaps4rails.js

--- lib/application_helper.rb
+++ lib/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
   
-  def gmaps4rails(builder, enable_css = true )
+  def gmaps4rails(builder, enable_css = true, enable_js = true )
     options = {
       "map_options" => { "auto_adjust" => true},
       "markers"     => { "data" => builder }
     }
-    render :partial => 'gmaps4rails/gmaps4rails', :locals => { :options => options, :enable_css => enable_css }
+    render :partial => 'gmaps4rails/gmaps4rails', :locals => { :options => options, :enable_css => enable_css, :enable_js => enable_js }
   end
   
-  def gmaps(options, enable_css = true )
-    render :partial => 'gmaps4rails/gmaps4rails', :locals => { :options => options, :enable_css => enable_css }
+  def gmaps(options, enable_css = true, enable_js = true )
+    render :partial => 'gmaps4rails/gmaps4rails', :locals => { :options => options, :enable_css => enable_css, :enable_js => enable_js }
   end
 
 end
