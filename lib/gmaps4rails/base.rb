abbc27351945e5f5ac622918f9657f9c0f84f8b9
apneadiving <apnea.diving.deep@gmail.com>
2011-06-22 23:22:50 +0200

    still refactoring

--- lib/gmaps4rails/base.rb
+++ lib/gmaps4rails/base.rb
@@ -1,7 +1,6 @@
 require 'net/http'
 require 'uri'
 require 'crack'
-require 'logger'
 
 module Gmaps4rails
   
@@ -43,38 +42,40 @@ module Gmaps4rails
      #send request to the google api to get the lat/lng
      request = geocoder + address + output
      url = URI.escape(request)
-     resp = Net::HTTP.get_response(URI.parse(url))
-     #parse result if result received properly
-     if resp.is_a?(Net::HTTPSuccess)             
-       #parse the json
-       parse = Crack::JSON.parse(resp.body)
-       #logger.debug "Google geocoding. Address: #{address}. Result: #{resp.body}"
-       #check if google went well
-       if parse["status"] == "OK"
-         return parse if raw == true
-         array = []
-         parse["results"].each do |result|
-           array << { 
-                      :lat => result["geometry"]["location"]["lat"], 
-                      :lng => result["geometry"]["location"]["lng"],
-                      :matched_address => result["formatted_address"],
-                      :bounds => result["geometry"]["bounds"],
-                      :full_data => result
-                     }
-         end
-         return array
-       else #status != OK
-         raise Gmaps4rails::GeocodeStatus, "The address you passed seems invalid, status was: #{parse["status"]}.
-         Request was: #{request}"
-       end #end parse status
-       
-     else #if not http success
-       raise Gmaps4rails::GeocodeNetStatus, "The request sent to google was invalid (not http success): #{request}.
-       Response was: #{resp}"           
-     end #end resp test
+     Gmaps4rails.handle_geocoding_response(request, Net::HTTP.get_response(URI.parse(url)), raw)
    end # end address valid
-  end #end geocode
+  end
   
+  def Gmaps4rails.handle_geocoding_response(request, response, raw)
+    #parse result if result received properly
+    if response.is_a?(Net::HTTPSuccess)             
+      #parse the json
+      parse = Crack::JSON.parse(response.body)
+      #check if google went well
+      if parse["status"] == "OK"
+        return parse if raw == true
+        array = []
+        parse["results"].each do |result|
+          array << { 
+                     :lat => result["geometry"]["location"]["lat"], 
+                     :lng => result["geometry"]["location"]["lng"],
+                     :matched_address => result["formatted_address"],
+                     :bounds => result["geometry"]["bounds"],
+                     :full_data => result
+                    }
+        end
+        return array
+      else #status != OK
+        raise Gmaps4rails::GeocodeStatus, "The address you passed seems invalid, status was: #{parse["status"]}.
+        Request was: #{request}"
+      end #end parse status
+      
+    else #if not http success
+      raise Gmaps4rails::GeocodeNetStatus, "The request sent to google was invalid (not http success): #{request}.
+      Response was: #{response}"           
+    end #end resp test
+  end
+   
   #output could be raw, pretty or clean
   def Gmaps4rails.destination(start_end, options={}, output="pretty")
    if start_end["from"].nil? || start_end["to"].empty?
@@ -86,43 +87,46 @@ module Gmaps4rails
      #send request to the google api to get the directions
      request = geocoder + dest_options + "&sensor=false"
      url = URI.escape(request)
-     resp = Net::HTTP.get_response(URI.parse(url))
-     #parse result if result received properly
-     if resp.is_a?(Net::HTTPSuccess)             
-       #parse the json
-       parse = Crack::JSON.parse(resp.body)
-       #check if google went well
-       if parse["status"] == "OK"
-        legs = []
-        #Each element in the legs array specifies a single leg of the journey from the origin to the destination in the calculated route
-        parse["routes"].first["legs"].each do |leg|
-          #delete coded polyline elements from legs and store it in polylines to make output cleaner
-          polylines = leg["steps"].map {|step| step.delete("polyline")} if output == "pretty" || output == "clean"
-          legs << {
-                    "duration"  => { "text" => leg["duration"]["text"], "value" => leg["duration"]["value"].to_f },
-                    "distance"  => { "text" => leg["distance"]["text"], "value" => leg["distance"]["value"].to_f },
-                    "steps"     => leg["steps"]
-                  }
-          if output == "pretty"
-            #polylines contain levels data, which are not that useful.
-            polylines.map{|poly| poly.delete("levels")}
-            #create valid json from all polylines, this could be directly passed to javascript for display
-            json = polylines.map { |poly| {"coded_array" => poly["points"]} }.to_json
-            #merge results in legs
-            legs.last.merge!({ "polylines" => json })
-          end
-        end
-        return legs
-       else #status != OK
-         raise Gmaps4rails::DirectionStatus, "The query you passed seems invalid, status was: #{parse["status"]}.
-         Request was: #{request}"
-       end #end parse status
-     else #if not http success
-       raise Gmaps4rails::DirectionNetStatus, "The request sent to google was invalid (not http success): #{request}.
-       Response was: #{resp}"           
-     end #end resp test
+     Gmaps4rails.handle_destination_response(request, Net::HTTP.get_response(URI.parse(url)), output)
    end # end origin + destination exist
   end #end destination
+  
+  #parse result if result received properly
+  def Gmaps4rails.handle_destination_response(request, response, output)
+    if response.is_a?(Net::HTTPSuccess)             
+      #parse the json
+      parse = Crack::JSON.parse(response.body)
+      #check if google went well
+      if parse["status"] == "OK"
+       legs = []
+       #Each element in the legs array specifies a single leg of the journey from the origin to the destination in the calculated route
+       parse["routes"].first["legs"].each do |leg|
+         #delete coded polyline elements from legs and store it in polylines to make output cleaner
+         polylines = leg["steps"].map {|step| step.delete("polyline")} if output == "pretty" || output == "clean"
+         legs << {
+                   "duration"  => { "text" => leg["duration"]["text"], "value" => leg["duration"]["value"].to_f },
+                   "distance"  => { "text" => leg["distance"]["text"], "value" => leg["distance"]["value"].to_f },
+                   "steps"     => leg["steps"]
+                 }
+         if output == "pretty"
+           #polylines contain levels data, which are not that useful.
+           polylines.map{|poly| poly.delete("levels")}
+           #create valid json from all polylines, this could be directly passed to javascript for display
+           json = polylines.map { |poly| {"coded_array" => poly["points"]} }.to_json
+           #merge results in legs
+           legs.last.merge!({ "polylines" => json })
+         end
+       end
+       return legs
+      else #status != OK
+        raise Gmaps4rails::DirectionStatus, "The query you passed seems invalid, status was: #{parse["status"]}.
+        Request was: #{request}"
+      end #end parse status
+    else #if not http success
+      raise Gmaps4rails::DirectionNetStatus, "The request sent to google was invalid (not http success): #{request}.
+      Response was: #{response}"           
+    end #end resp test
+  end
   
   
   def Gmaps4rails.filter(data)
