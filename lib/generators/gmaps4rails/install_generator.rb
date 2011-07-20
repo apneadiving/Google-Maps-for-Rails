639797884ffa9230929e6f73dd060db8d0a0860a
apneadiving <apnea.diving.deep@gmail.com>
2011-07-20 13:56:07 +0200

    changed generator

--- lib/generators/gmaps4rails/install_generator.rb
+++ lib/generators/gmaps4rails/install_generator.rb
@@ -7,10 +7,10 @@ module Gmaps4rails
 
       def copy_locale
         copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.js'
-        copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.googlemaps.js'
-        copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.bing.js'
-        copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.openlayers.js'
-        copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.mapquest.js'
+        copy_file '../../../public/javascripts/gmaps4rails.googlemaps.js', 'public/javascripts/gmaps4rails.googlemaps.js'
+        copy_file '../../../public/javascripts/gmaps4rails.bing.js', 'public/javascripts/gmaps4rails.bing.js'
+        copy_file '../../../public/javascripts/gmaps4rails.openlayers.js', 'public/javascripts/gmaps4rails.openlayers.js'
+        copy_file '../../../public/javascripts/gmaps4rails.mapquest.js', 'public/javascripts/gmaps4rails.mapquest.js'
         copy_file '../../../public/stylesheets/gmaps4rails.css', 'public/stylesheets/gmaps4rails.css'
         copy_file '../../../public/images/marker.png', 'public/images/marker.png'
       end
