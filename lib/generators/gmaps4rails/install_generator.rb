7f1b86de327e41823afeeaa24a68ed5709311795
cameel <cameel2@gmail.com>
2011-07-22 20:03:46 +0200

    Use asset pipeline in Rails 3.1
    - Move javascripts, stylesheets and images to app/assets. This way they don't need to be copied to the application directory in Rails >= 3.1 and can still be copied by the generator in earlier versions
    - Update the generator to look for assets in app/assets rather than in public/
    - Make generator copy files to public/assets if Rails version is >= 3.1
    - Update file locations in gemspec

--- lib/generators/gmaps4rails/install_generator.rb
+++ lib/generators/gmaps4rails/install_generator.rb
@@ -6,17 +6,42 @@ module Gmaps4rails
       desc 'Creates a Gmaps4rails initializer and copies the assets to the public folder.'
 
       def copy_locale
-        copy_file '../../../public/javascripts/gmaps4rails.js', 'public/javascripts/gmaps4rails.js'
-        copy_file '../../../public/javascripts/gmaps4rails.googlemaps.js', 'public/javascripts/gmaps4rails.googlemaps.js'
-        copy_file '../../../public/javascripts/gmaps4rails.bing.js', 'public/javascripts/gmaps4rails.bing.js'
-        copy_file '../../../public/javascripts/gmaps4rails.openlayers.js', 'public/javascripts/gmaps4rails.openlayers.js'
-        copy_file '../../../public/javascripts/gmaps4rails.mapquest.js', 'public/javascripts/gmaps4rails.mapquest.js'
-        copy_file '../../../public/stylesheets/gmaps4rails.css', 'public/stylesheets/gmaps4rails.css'
-        copy_file '../../../public/images/marker.png', 'public/images/marker.png'
+        src_dir   = '../../../app/assets'
+        dest_dirs = public_asset_paths
+
+        copy_file src_dir + '/javascripts/gmaps4rails.js',            dest_dirs[:js]  + '/gmaps4rails.js'
+        copy_file src_dir + '/javascripts/gmaps4rails.googlemaps.js', dest_dirs[:js]  + '/gmaps4rails.googlemaps.js'
+        copy_file src_dir + '/javascripts/gmaps4rails.bing.js',       dest_dirs[:js]  + '/gmaps4rails.bing.js'
+        copy_file src_dir + '/javascripts/gmaps4rails.openlayers.js', dest_dirs[:js]  + '/gmaps4rails.openlayers.js'
+        copy_file src_dir + '/javascripts/gmaps4rails.mapquest.js',   dest_dirs[:js]  + '/gmaps4rails.mapquest.js'
+        copy_file src_dir + '/stylesheets/gmaps4rails.css',           dest_dirs[:css] + '/gmaps4rails.css'
+        copy_file src_dir + '/images/marker.png',                     dest_dirs[:img] + '/marker.png'
       end
 
       def show_readme
         readme 'README' if behavior == :invoke
+      end
+
+  private
+      def rails_version
+        property = Rails::Info.properties.find { |p| p[0] == "Rails version" }
+        return property[1]
+      end
+
+      def public_asset_paths
+        if Gem::Version.new(rails_version) < Gem::Version.new("3.1.0.beta")
+          return {
+            :js  => 'public/javascripts',
+            :css => 'public/stylesheets',
+            :img => 'public/images'
+          }
+        else
+          return {
+            :js  => 'public/assets',
+            :css => 'public/assets',
+            :img => 'public/assets'
+          }
+        end
       end
     end
   end
