100cd66c2fcfa3981e9b5584ce05f6bafe81cf18
apneadiving <apnea.diving.deep@gmail.com>
2011-09-22 23:09:00 +0200

    basic test in dummy app

--- test/dummy30/public/javascripts/application.js
+++ test/dummy30/public/javascripts/application.js
@@ -1,3 +1,3 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-https://github.com/apneadiving/Google-Maps-for-Rails/blob/master/lib/gmaps4rails.rb
+//https://github.com/apneadiving/Google-Maps-for-Rails/blob/master/lib/gmaps4rails.rb
