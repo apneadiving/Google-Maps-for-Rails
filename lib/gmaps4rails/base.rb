c3b642c0ecdcefeb4dc7a7c692a684d089f7dda3
Nihad Abbasov <narkoz.2008@gmail.com>
2011-10-30 16:32:41 +0400

    use protocol relative urls for google-maps to avoid security warnings

--- lib/gmaps4rails/base.rb
+++ lib/gmaps4rails/base.rb
@@ -120,7 +120,7 @@ module Gmaps4rails
    if address.nil? || address.empty?
      raise Gmaps4rails::GeocodeInvalidQuery, "You must provide an address"
    else #coordinates are valid
-     geocoder = "http://maps.googleapis.com/maps/api/geocode/json?language=#{lang}&address="
+     geocoder = "//maps.googleapis.com/maps/api/geocode/json?language=#{lang}&address="
      output = "&sensor=false"
      #send request to the google api to get the lat/lng
      request = geocoder + address + output
@@ -139,7 +139,7 @@ module Gmaps4rails
    if start_end["from"].nil? || start_end["to"].empty?
      raise Gmaps4rails::DirectionInvalidQuery, "Origin and destination must be provided in a hash as first argument"
    else #great, we have stuff to work with
-     geocoder = "http://maps.googleapis.com/maps/api/directions/json?origin=#{start_end["from"]}&destination=#{start_end["to"]}"
+     geocoder = "//maps.googleapis.com/maps/api/directions/json?origin=#{start_end["from"]}&destination=#{start_end["to"]}"
      #if value is an Array, it means it contains the waypoints, otherwise it's chained normally
      dest_options = options.empty? ? "" : "&" + options.map {|k,v| v.is_a?(Array) ? k + "=" + v * ("|") : k + "=" + v }*("&") 
      #send request to the google api to get the directions
