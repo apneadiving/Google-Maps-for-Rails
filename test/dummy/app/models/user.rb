fdb949d4c056051630efa01441a85b8b0080f6b9
apneadiving <apnea.diving.deep@gmail.com>
2012-03-07 21:54:11 +0100

    bump google maps api version

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -8,14 +8,14 @@ class User < ActiveRecord::Base
       address
     end
     
-      #  # #   
+       # #   
       #  def gmaps4rails_marker_picture
       #    {
-      #    "width" => "32",
-      #    "height" => "32",
+      #    # "width" => "32",
+      #    # "height" => "32",
       # # "marker_anchor" => [2, 3],
       #   # "picture" =>  "https://secure.gravatar.com/avatar/808bec1c640143bd7091888d9edfb2f2.png",
-      # #   "rich_marker" => "<div class='my-marker'>It works!<img height='30' width='30' src='http://farm4.static.flickr.com/3212/3012579547_097e27ced9_m.jpg'/></div>",
+      #    "rich_marker" => "<div class='my-marker'>It works!<img height='30' width='30' src='http://farm4.static.flickr.com/3212/3012579547_097e27ced9_m.jpg'/></div>",
       # #   "shadow_picture" => "http://www.blankdots.com/img/github-32x32.png",
       # #   "shadow_height" => 40, 
       # #   "shadow_width" => 50,
