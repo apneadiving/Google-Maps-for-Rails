777ae46ef9f5a0be7228554e4d99f31a937a07a8
apneadiving <apnea.diving.deep@gmail.com>
2012-05-01 00:13:51 +0200

    updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,6 @@ Spork.prefork do
   
  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each   {|f| require f }
  FactoryGirl.definition_file_paths = [ File.join(Rails.root, '../factories') ]
- FactoryGirl.find_definitions
  
   RSpec.configure do |config|
     config.treat_symbols_as_metadata_keys_with_true_values = true
