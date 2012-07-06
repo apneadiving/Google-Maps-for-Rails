279fd5c783ba44671381973d781813428402102d
Andrewglass1 <Andrewglass1@gmail.com>
2012-07-06 16:03:56 -0400

    added animation functions and tests

--- public/javascripts/gmaps4rails/gmaps4rails.googlemaps.js
+++ public/javascripts/gmaps4rails/gmaps4rails.googlemaps.js
@@ -199,6 +199,39 @@
       return _results;
     };
 
+    Gmaps4RailsGoogle.prototype.bounceMarkers = function() {
+      var marker, _i, _len, _ref, _results;
+      _ref = this.markers;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        marker = _ref[_i];
+        _results.push(this.bounceMarker(marker));
+      }
+      return _results;
+    };
+
+    Gmaps4RailsGoogle.prototype.dropMarkers = function() {
+      var marker, _i, _len, _ref, _results;
+      _ref = this.markers;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        marker = _ref[_i];
+        _results.push(this.dropMarker(marker));
+      }
+      return _results;
+    };
+
+    Gmaps4RailsGoogle.prototype.freezeMarkers = function() {
+      var marker, _i, _len, _ref, _results;
+      _ref = this.markers;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        marker = _ref[_i];
+        _results.push(this.freezeMarker(marker));
+      }
+      return _results;
+    };
+
     Gmaps4RailsGoogle.prototype.clearMarker = function(marker) {
       return marker.serviceObject.setMap(null);
     };
