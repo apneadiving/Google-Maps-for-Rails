f30a13a3673d7dce7106e4264c4b7bc888387b64
apneadiving <apnea.diving.deep@gmail.com>
2012-08-06 22:48:50 +0200

    conditionnal model

--- spec/dummy/app/models/place.rb
+++ spec/dummy/app/models/place.rb
@@ -1,12 +1,16 @@
-require 'mongoid'
-
-class Place  
-  include Mongoid::Document
-  include Gmaps4rails::ActsAsGmappable
-
-  acts_as_gmappable :address => :address, :position => :pos
+if RUBY_VERSION == "1.9.3"
   
-  field :pos,      :type => Array
-  field :address,  :type => String
-  field :gmaps,    :type => Boolean
+  require 'mongoid'
+
+  class Place  
+    include Mongoid::Document
+    include Gmaps4rails::ActsAsGmappable
+
+    acts_as_gmappable :address => :address, :position => :pos
+    
+    field :pos,      :type => Array
+    field :address,  :type => String
+    field :gmaps,    :type => Boolean
+  end
+
 end
