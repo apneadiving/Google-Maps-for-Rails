e118347f91ea335ef8949ee92308476c6fed26ce
apneadiving <apnea.diving.deep@gmail.com>
2012-08-15 15:07:23 +0200

    adapted generator

--- lib/generators/gmaps4rails/install_generator.rb
+++ lib/generators/gmaps4rails/install_generator.rb
@@ -6,42 +6,37 @@ module Gmaps4rails
       desc 'Creates a Gmaps4rails initializer and copies the assets to the public folder.'
 
       def copy_locale
-        if Rails::VERSION::MINOR >= 1
-          copy_file "#{source_assets_base_path}gmaps4rails.base.js.coffee",       "#{destination_assets_base_path}gmaps4rails.base.js.coffee"
-          copy_file "#{source_assets_base_path}gmaps4rails.googlemaps.js.coffee", "#{destination_assets_base_path}gmaps4rails.googlemaps.js.coffee"
-          copy_file "#{source_assets_base_path}gmaps4rails.openlayers.js.coffee", "#{destination_assets_base_path}gmaps4rails.openlayers.js.coffee"
-          copy_file "#{source_assets_base_path}gmaps4rails.bing.js.coffee",       "#{destination_assets_base_path}gmaps4rails.bing.js.coffee"
-          copy_file "#{source_assets_base_path}gmaps4rails.mapquest.js.coffee",   "#{destination_assets_base_path}gmaps4rails.mapquest.js.coffee"        
+        if false #Rails::VERSION::MINOR >= 1
+          directory assets_source_path, assets_destination_path
           copy_file "../../../public/stylesheets/gmaps4rails.css", "app/assets/stylesheets/gmaps4rails.css"
         else
-        #I don't copy manifests, kind of useless
-          copy_file "#{source_js_base_path}gmaps4rails.base.js",       "#{destination_js_base_path}gmaps4rails.base.js"
-          copy_file "#{source_js_base_path}gmaps4rails.googlemaps.js", "#{destination_js_base_path}gmaps4rails.googlemaps.js"
-          copy_file "#{source_js_base_path}gmaps4rails.bing.js",       "#{destination_js_base_path}gmaps4rails.bing.js"
-          copy_file "#{source_js_base_path}gmaps4rails.openlayers.js", "#{destination_js_base_path}gmaps4rails.openlayers.js"
-          copy_file "#{source_js_base_path}gmaps4rails.mapquest.js",   "#{destination_js_base_path}gmaps4rails.mapquest.js"
-          copy_file "../../../public/stylesheets/gmaps4rails.css",     "public/stylesheets/gmaps4rails.css"
+          %w( base google openlayers all ).each do |filename|
+            copy_file js_source_path(filename), js_destination_path(filename)
+          end
+          copy_file "../../../public/stylesheets/gmaps4rails.css", "public/stylesheets/gmaps4rails.css"
         end
-      end
-      
-      def source_assets_base_path
-        '../../../app/assets/javascripts/gmaps4rails/'
-      end
-      
-      def destination_assets_base_path
-        'app/assets/javascripts/gmaps4rails/'
-      end
-      
-      def source_js_base_path
-        "../../../public/javascripts/gmaps4rails/"
-      end
-      
-      def destination_js_base_path
-        "public/javascripts/gmaps4rails/"
       end
 
       def show_readme
         readme 'README' if behavior == :invoke
+      end
+
+      private
+
+      def assets_source_path
+        '../../../vendor/assets/javascripts/gmaps4rails/'
+      end
+      
+      def assets_destination_path
+        'app/assets/javascripts/gmaps4rails'
+      end
+      
+      def js_source_path(filename)
+        "../../../public/javascripts/gmaps4rails/#{filename}.js"
+      end
+      
+      def js_destination_path(filename)
+        "public/javascripts/gmaps4rails/#{filename}.js"
       end
     end
   end
--- lib/tasks/gmaps4rails_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :gmaps4rails do
-#   # Task goes here
-# end
